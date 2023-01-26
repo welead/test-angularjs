00a88679980f122762f2b924714098063cfdf850
welead <30601062+welead@users.noreply.github.com>
2023-01-26 09:35:14 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Document sans nom</title>
+</head>
+
+<body>
+<form>
+ 
+  <p>
+    <label for="firstname">First Name </label>
+    <input type="text" id="firstname" name="firstname">
+  </p>
+ 
+  <p>
+    <label for="lastname">Last Name </label>
+    <input type="text" id="lastname" name="lastname">
+  </p>
+ 
+  <p>
+    <label for="email">Email </label>
+    <input type="text" id="email" name="email">
+  </p>
+	 <p>
+    <label for="age">Age </label>
+    <input type="text" id="age" name="email">
+  </p>
+	 <p>
+    <label for="age">Date of birth </label>
+    <input type="text" id="age" name="dof">
+  </p>
+ 
+  <p>
+    <label for="gender">Geneder </label>
+    <input type="radio" value="male" id="gender" name="gender"> Male
+    <input type="radio" value="female" id="gender" name="gender"> Female
+  </p>
+ 
+  <p>
+    <label for="isMarried">Married </label>
+    <input type="checkbox" id="isMarried" name="isMarried">
+  </p>
+ 
+ 
+  <p>
+    <label for="country">country </label>
+    <select id="country" name="country">
+		      <option value="" selected>select one please</option>
+      <option value="1">India</option>
+      <option value="2">USA</option>
+      <option value="3">England</option>
+      <option value="4">Singapore</option>
+    </select>
+  </p>
+ 
+  <p>
+    <button type="submit">Submit</button>
+  </p>
+ 
+</form>
+	</body>
+</html>
+	
+</body>
+</html>
